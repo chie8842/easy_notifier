aec300770660abff93bd9385534a1d878f5325e9
chie8842 <chie-hayashida@cookpad.com>
2018-03-03 17:23:06 +0900

    version 1.4.0 -> 1.5.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import os
 from setuptools import setup, find_packages
 
 
-__version__ = '0.1.4'
+__version__ = '0.1.5'
 __author__ = 'Chie Hayashida'
 __author_email__ = 'chie-hayashida@cookpad.com'
 __classifiers__ = (
